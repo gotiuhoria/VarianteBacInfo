0b4052fb3fff0530d4ae74ba5b1e182d324b3760
Horia Gotiu <gotiu.horia@gmail.com>
2021-01-05 00:56:59 +0200

    Frontend and Backend communicating through JWT

---
+++ VarianteBacInfo-1.0/VBIBackend/Controllers/SecurityController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PtcApi.Security;
+using PtcApi.Model;
+
+namespace PtcApi.Controllers
+{
+  [Route("api/[controller]")]
+  public class SecurityController : Controller
+  {
+
+	private JwtSettings _settings = null;
+
+	public SecurityController(JwtSettings settings)
+	{
+		_settings = settings;
+	}
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody]AppUser user)
+    {
+		AppUserAuth auth = new AppUserAuth();
+		SecurityManager mgr = new SecurityManager(_settings);
+
+		auth = mgr.ValidateUser(user);
+
+		IActionResult ret;
+		if (auth.IsAuthenticated)
+		{
+			ret = StatusCode(StatusCodes.Status200OK, auth);
+		}
+		else
+		{
+			ret = StatusCode(StatusCodes.Status404NotFound,
+								"Invalid User Name/Password.");
+		}
+
+		return ret;
+    }
+  }
+}
